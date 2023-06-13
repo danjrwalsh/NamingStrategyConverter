5814881eb6ac69a75e3c13df85fbcf5bab881ad2
Dan Walsh <dan@danwalsh.ca>
2023-06-13 17:18:57 -0400

    Moved `NamingStrategyExample` to benchmarks project

---
+++ Benchmarks/NamingStrategyExample.cs
@@ -0,0 +1,11 @@
+namespace DanWalsh.NamingStrategyConverter.Benchmarks;
+
+public static class NamingStrategyExample
+{
+    public const string PascalCase = "PascalCase";
+    public const string CamelCase = "camelCase";
+    public const string KebabCase = "kebab-case";
+    public const string SnakeCase = "snake_case";
+    public const string ScreamingSnakeCase = "SCREAMING_SNAKE_CASE";
+    public const string ScreamingKebabCase = "SCREAMING-KEBAB-CASE";
+}
