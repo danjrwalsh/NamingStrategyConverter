f615672c065f9888c8de25013f089ee59c950002
Dan Walsh <dan@danwalsh.ca>
2023-06-15 01:45:28 -0400

    Added naming policies

---
+++ src/NamingStrategyConverter.SystemTextJson/UpperSnakeCaseNamingPolicy.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+
+namespace DanWalsh.NamingStrategyConverter.SystemTextJson;
+
+public class UpperSnakeCaseNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name) => name.ToUpperSnakeCase();
+}
